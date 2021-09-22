f5fec011091542bff724f45fd0afb0948c45c137
viniciusboliveira <vinicius.fb.oliveira@gmail.com>
2021-09-22 20:18:03 -0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,55 @@
+import pandas as pd
+import win32com.client as win32
+
+# importar a base de dados
+tabela_vendas = pd.read_excel('Vendas.xlsx')
+
+# visualizar a base de dados
+pd.set_option('display.max_columns', None)
+print(tabela_vendas)
+
+print('-' *50)
+
+# faturamento por loja
+faturamento = tabela_vendas[['ID Loja', 'Valor Final']].groupby('ID Loja').sum()
+print(faturamento)
+
+# quantidade de produtos vendidos por loja
+quantidade = tabela_vendas[['ID Loja','Quantidade']].groupby('ID Loja').sum()
+print(quantidade)
+
+print('-' *50)
+
+# ticket mpedio por produto em cada loja
+ticket_medio = (faturamento['Valor Final'] / quantidade['Quantidade']).to_frame()
+ticket_medio = ticket_medio.rename(columns={0: 'Ticket Médio'})
+print(ticket_medio)
+
+
+# enviar um email com o relatório
+
+outlook = win32.Dispatch('outlook.application')
+mail = outlook.CreateItem(0)
+mail.To = 'vinicius.fb.oliveira@gmail.com'
+mail.Subject = 'Assunto teste'
+mail.HTMLBody = f'''
+<p>Prezados,</p>
+
+<p>Segue relatório de vendas por cada loja.</p>
+
+<p>Faturamento:</p>
+{faturamento.to_html(formatters={'Valor Final': 'R${:,.2f}'.format})}
+
+<p>Quantidade Vendida:</p>
+{quantidade.to_html()}
+
+<p>Ticket médio dos produtos em cada loja:</p>
+{ticket_medio.to_html(formatters={'Ticket Médio': 'R${:,.2f}'.format})}
+
+<p>Quaquer dúvida, estamos a disposição.</p>
+
+Att,
+'''
+#'R${:,.2f} = Separador de milhão, centena, dezena, etc..
+
+mail.Send()
